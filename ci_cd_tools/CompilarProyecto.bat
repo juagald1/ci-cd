15bdd282b9824c797daa1b7285d5d8ec773fb188
jgalbis <86072063+juagald1@users.noreply.github.com>
2023-11-20 17:34:18 +0100

    9

--- ci_cd_tools/CompilarProyecto.bat
+++ ci_cd_tools/CompilarProyecto.bat
@@ -11,7 +11,7 @@ rem C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "%workspace%" -applicatio
 
 rem Ejecutar el comando '>' y guarda la salida en archivo temporal
 set "archivoTemporal=temp_output.txt"
-C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "%workspace%" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "%proyecto%" -ccs.configuration Debug
+archivoTemporal = C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "%workspace%" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "%proyecto%" -ccs.configuration Debug
 
 rem Imprimir .txt
 type "%archivoTemporal%"
