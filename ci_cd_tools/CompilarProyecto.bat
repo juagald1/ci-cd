6a8928b7e73e8a6996a04506d72a2c9a88e830cf
jgalbis <86072063+juagald1@users.noreply.github.com>
2023-11-21 11:41:44 +0100

    Update CompilarProyecto.bat

--- ci_cd_tools/CompilarProyecto.bat
+++ ci_cd_tools/CompilarProyecto.bat
@@ -2,19 +2,11 @@
 set workspace=%1
 set proyecto=%2
 
-REM Se añde la ruta completa de (eclipse) a eclipsec.exe, ya que Jenkins no permite ejecutar directamente variables de path añadidas en el PC local
-REM Añade el proyecto al workspace y lo sobreescribe si ya existe para evitar errores
-REM C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "C:\Users\Usuario\workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location C:\Users\Usuario\Documents\GitHub\ci-cd -ccs.overwrite
-REM C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data %workspace% -application com.ti.ccstudio.apps.projectImport -ccs.location C:\Users\Usuario\Documents\GitHub\ci-cd -ccs.overwrite
-
 REM Crea archivo auxiliar para almacenar log de manera temporal
 set "archivoTemporal=temp_output.txt"	
 
 REM Se añde la ruta completa de (eclipse) a eclipsec.exe, ya que Jenkins no permite ejecutar directamente variables de path añadidas en el PC local
 C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data %workspace% -application com.ti.ccstudio.apps.projectBuild -ccs.projects %proyecto% -ccs.configuration Debug > "%archivoTemporal%"
-
-REM Copiado como ejemplo de ruta completa:
-REM C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "C:\Users\Usuario\workspace" -application com.ti.ccstudio.apps.projectBuild -ccs.projects test_28027 -ccs.configuration Debug > "%archivoTemporal%"
 
 REM Muestra archivo temporal, resultado compilacion
 type "%archivoTemporal%"
