562c1b7f560ae65d77898466757aa52a29d1030a
jgalbis <86072063+juagald1@users.noreply.github.com>
2023-11-21 09:00:34 +0100

    Update CompilarProyecto.bat

--- ci_cd_tools/CompilarProyecto.bat
+++ ci_cd_tools/CompilarProyecto.bat
@@ -1,22 +1,21 @@
 @echo off
-REM 1 Ruta completa de eclipsec, Jenkins no permite ejecutar directamente variables de path añadidas en el PC
+set workspace=%1
+set proyecto=%2
+set "archivoTemporal=temp_output.txt"	
+
+REM 1 Ruta completa de eclipsec, Jenkins no permite ejecutar directamente variables de path añadidas en el PC local
 REM 2 Ruta a Workspace (si el proyecto ha sido compilado al menos 1 vez, lo añade automaticamente a la carpeta .metadata de workspace)
 REM 3 Nombre del proyecto (-ccs.projects)
 REM 4 Carpeta Debug de proyecto (-ccs.configuration)
 
-set workspace=%1
-set proyecto=%2
-
-rem C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "%workspace%" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "%proyecto%" -ccs.configuration Debug
-
-rem Ejecutar el comando '>' y guarda la salida en archivo temporal
-set "archivoTemporal=temp_output.txt"
+rem Ejecutar el comando y guarda la salida en .txt temporal con '>'
 C:\ti\ccs1200\ccs\eclipse\eclipsec -noSplash -data "%workspace%" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "%proyecto%" -ccs.configuration Debug > "%archivoTemporal%"
 
-rem Imprimir .txt
+rem Muestra archivo temporal
 type "%archivoTemporal%"
 
-rem Buscar una palabra específica dentro del archivo temporal
+rem En funcion de la cadena de caracterers generada, retorna un codigo que Jenkins interpretara
+rem Finalmente elimina el archivo temporal
 set "palabraABuscar=CCS headless build complete! 0 out of 0 projects have errors."
 findstr /C:"%palabraABuscar%" "%archivoTemporal%" > nul
 
