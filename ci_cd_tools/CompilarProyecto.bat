4f0840447171b475be3cba4e46e57d3ed5cadc5d
jgalbis <86072063+juagald1@users.noreply.github.com>
2023-11-21 09:36:44 +0100

    o

--- ci_cd_tools/CompilarProyecto.bat
+++ ci_cd_tools/CompilarProyecto.bat
@@ -1,15 +1,18 @@
 @echo off
 set workspace=%1
 set proyecto=%2
-set "archivoTemporal=temp_output.txt"	
 
 REM 1 Ruta completa de eclipsec, Jenkins no permite ejecutar directamente variables de path añadidas en el PC local
 REM 2 Ruta a Workspace (si el proyecto ha sido compilado al menos 1 vez, lo añade automaticamente a la carpeta .metadata de workspace)
 REM 3 Nombre del proyecto (-ccs.projects)
 REM 4 Carpeta Debug de proyecto (-ccs.configuration)
+rem C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data C:\Users\Usuario\workspace -application com.ti.ccstudio.apps.projectBuild -ccs.projects test_28027 -ccs.configuration Debug
 
 rem Ejecutar el comando y guarda la salida en .txt temporal con '>'
+set "archivoTemporal=temp_output.txt"	
 C:\ti\ccs1200\ccs\eclipse\eclipsec -noSplash -data "%workspace%" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "%proyecto%" -ccs.configuration Debug > "%archivoTemporal%"
+
+
 
 rem Muestra archivo temporal
 type "%archivoTemporal%"
