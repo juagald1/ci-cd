c8bc34044cb06e07c232b1a0774368d2ab038cfb
jgalbis <86072063+juagald1@users.noreply.github.com>
2023-11-21 11:19:10 +0100

    Actualizado

--- ci_cd_tools/CompilarProyecto.bat
+++ ci_cd_tools/CompilarProyecto.bat
@@ -2,23 +2,24 @@
 set workspace=%1
 set proyecto=%2
 
-REM 1 Ruta completa de eclipsec, Jenkins no permite ejecutar directamente variables de path añadidas en el PC local
-REM 2 Ruta a Workspace (si el proyecto ha sido compilado al menos 1 vez, lo añade automaticamente a la carpeta .metadata de workspace)
-REM 3 Nombre del proyecto (-ccs.projects)
-REM 4 Carpeta Debug de proyecto (-ccs.configuration)
-rem C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data C:\Users\Usuario\workspace -application com.ti.ccstudio.apps.projectBuild -ccs.projects test_28027 -ccs.configuration Debug
+REM Se añde la ruta completa de (eclipse) a eclipsec.exe, ya que Jenkins no permite ejecutar directamente variables de path añadidas en el PC local
+REM Añade el proyecto al workspace y lo sobreescribe si ya existe para evitar errores
+REM C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "C:\Users\Usuario\workspace" -application com.ti.ccstudio.apps.projectImport -ccs.location C:\Users\Usuario\Documents\GitHub\ci-cd -ccs.overwrite
+C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data %workspace% -application com.ti.ccstudio.apps.projectImport -ccs.location C:\Users\Usuario\Documents\GitHub\ci-cd -ccs.overwrite
 
-rem Ejecutar el comando y guarda la salida en .txt temporal con '>'
+REM Crea archivo auxiliar para almacenar log de manera temporal
 set "archivoTemporal=temp_output.txt"	
-C:\ti\ccs1200\ccs\eclipse\eclipsec -noSplash -data "%workspace%" -application com.ti.ccstudio.apps.projectBuild -ccs.projects "%proyecto%" -ccs.configuration Debug > "%archivoTemporal%"
 
+REM Se añde la ruta completa de (eclipse) a eclipsec.exe, ya que Jenkins no permite ejecutar directamente variables de path añadidas en el PC local
+C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data %workspace% -application com.ti.ccstudio.apps.projectBuild -ccs.projects %proyecto% -ccs.configuration Debug > "%archivoTemporal%"
 
+REM Copiado como ejemplo de ruta completa:
+REM C:\ti\ccs1120\ccs\eclipse\eclipsec -noSplash -data "C:\Users\Usuario\workspace" -application com.ti.ccstudio.apps.projectBuild -ccs.projects test_28027 -ccs.configuration Debug > "%archivoTemporal%"
 
-rem Muestra archivo temporal
+REM Muestra archivo temporal, resultado compilacion
 type "%archivoTemporal%"
 
-rem En funcion de la cadena de caracterers generada, retorna un codigo que Jenkins interpretara
-rem Finalmente elimina el archivo temporal
+REM En funcion de la cadena de caracteres generada, retorna codigo (0,1,2) que Jenkins interpretara. Finalmente se elimina el archivo temporal
 set "palabraABuscar=CCS headless build complete! 0 out of 0 projects have errors."
 findstr /C:"%palabraABuscar%" "%archivoTemporal%" > nul
 
